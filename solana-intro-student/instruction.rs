6bd65ee686a8a59bfac8eff38c29fc3f6b4fb4b0
brknarsy <ixoth@DESKTOP-ETTJ0UB.localdomain>
2023-02-13 11:17:07 +0300

    my first program

---
+++ solana-intro-student/instruction.rs
@@ -0,0 +1,30 @@
+use solana_program::{program_error::ProgramError};
+use borsh::{BorshDeserialize};
+
+pub enum StudentInstruction {
+    CreateStudent {
+        name: String,
+        message: String
+    }
+}
+
+#[derive(BorshDeserialize)]
+struct StudentInstructionPayload {
+    name: String,
+    message: String
+}
+
+impl StudentInstruction {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
+        let (&variant, rest) => input.split_first().ok_or(ProgramError::InvalidInstructionData)?;
+        let payload = StudentInstructionPaylaod::try_from_slice(rest).unwrap();
+
+        Ok(match variant {
+            0 => Self::CreateStudent {
+                name: payload.name,
+                message: payload.message
+            },
+            _ => return Err(ProgramError::InvalidInstructionData)
+        })
+    }
+}
---
+++ solana-intro-student/lib.rs
@@ -0,0 +1,39 @@
+use solana_program::{
+    account_info::AccountInfo,
+    entrypoint,
+    entrypoint::ProgramResult,
+    pubkey::Pubkey,
+    msg
+};
+
+pub mod instruction;
+use instruction::{StudentInstruction};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    account_info: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let instruction = StudentInstruction::unpack(instruction_data)?;
+    match instruction {
+        CreateStudent {name, message} => {
+            create_student(program_id, account_info, name, message);
+        }
+    }
+    Ok(())
+}
+
+pub fn create_student (
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    name: String,
+    message: String,
+) -> ProgramResult {
+    msg!("Creating Student");
+
+    msg!("Student Name: {}", name);
+    msg!("Student Message: {}", message);
+    Ok(())
+}
